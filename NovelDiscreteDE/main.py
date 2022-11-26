023322b545e1a0fe147fd6e974669ef298fdceac
Icaro Felipe de Lima Ribeiro <iribeiro1@lenovo.com>
2022-11-26 15:45:14 -0300

    Adding mappers, file readers, population and graph

--- NovelDiscreteDE/main.py
+++ NovelDiscreteDE/main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import tsplib95
+from graph import Graph
+from novel_de import NovelDiscreteDE
+from time import time
 
 
-# Press the green button in the gutter to run the script.
+problem = tsplib95.load('a280.tsp')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    start = time()
+    problem = tsplib95.load('a280.tsp')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    population_size = 100
+    graph = Graph(problem)
+
+    ndde = NovelDiscreteDE(graph, population_size=population_size)
+    for individual in ndde.population:
+        print(individual.to_string())
+
+    end = time()
+    print("--- %s seconds ---" % (end - start))
